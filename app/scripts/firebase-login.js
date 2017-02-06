febb99629a212b12cdd78d7a59d9977da6db0cea
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-06 09:19:14 +0100

    Add firebase initialize script to start login

---
+++ app/scripts/firebase-login.js
@@ -0,0 +1,10 @@
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBaKY9uSwdZlRJhAuyTV2Kae-BLAnss8ew",
+authDomain: "amugikaproyects2015.firebaseapp.com",
+databaseURL: "https://amugikaproyects2015.firebaseio.com",
+storageBucket: "amugikaproyects2015.appspot.com",
+messagingSenderId: "906106841423"
+};
+firebase.initializeApp(config);
